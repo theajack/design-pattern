8a3825c9c513ce4d41880e951d714648a60a787f
tackchen <tackchen@tencent.com>
2022-08-03 21:42:56 +0800

    feat: initial commit

---
+++ scripts/dev/bundle.js
@@ -0,0 +1,9 @@
+// src/index.ts
+console.log("Hello world!");
+var src_default = "Hello Ebuild!";
+
+// scripts/dev/index.ts
+var win = window;
+win.main = src_default;
+console.log(src_default);
+//# sourceMappingURL=bundle.js.map
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: tackchen
+ * @Date: 2022-08-03 20:33:13
+ * @Description: Coding something
+ */
+console.log('Hello world!');
+
+export default 'Hello Ebuild!';
